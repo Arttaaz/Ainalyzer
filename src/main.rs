c314bb38f83ecf988477414b740df27b7db37c97
Arttaaz <arttaaz@gmail.com>
2020-08-19 19:32:31 +0200

    decided to use druid

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,35 @@
-use iced::{widget::canvas::Canvas, widget::canvas::Layer, Command, Application, Subscription, Element, Container, Length, Size};
-use std::sync::Arc;
+use druid::widget::{Align, Flex, Label, Spinner};
+use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WindowDesc};
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const WINDOW_TITLE: LocalizedString<HelloWidget> = LocalizedString::new("AInalyzer!");
 
-}
-
-
-struct App {}
-
-impl Application for App {
-
-    type Executor = iced::executor::Default;
-    type Message = Message;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Self, Command<Message>) {
-        (
-            App {},
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("AInalyzer")
-    }
-
-    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
-        Command::none()
-    }
-
-    fn subscription(&self) -> Subscription<Self::Message> {
-        Subscription::none()
-    }
-
-    fn view(&mut self) -> Element<'_, Self::Message> {
-        let canvas = Canvas::new()
-        .width(400.into())
-        .height(400.into());
-
-        Container::new(canvas).width(Length::Fill)
-            .height(Length::Fill)
-            .padding(20)
-            .center_x()
-            .center_y()
-            .into()
-    }
-
+#[derive(Debug, Clone, Data, Lens)]
+struct HelloWidget {
+    text: String,
 }
 
 fn main() {
-    App::run(iced::Settings::default());
+   let main_window = WindowDesc::new(build_root_widget)
+        .title(WINDOW_TITLE)
+        .window_size((400.0, 400.0));
+
+    AppLauncher::with_window(main_window)
+        .launch(HelloWidget{
+            text: "loading the game analyzer app. Trust me it is loading.".to_string(),
+        })
+        .expect("failed to launch app");
+}
+
+fn build_root_widget() -> impl Widget<HelloWidget> {
+    let label = Label::new(|data: &HelloWidget, _env: &Env| format!("{}", data.text));
+
+    let loading = Spinner::new();
+
+    let layout = Flex::column()
+        .with_child(label)
+        .with_spacer(VERTICAL_WIDGET_SPACING)
+        .with_child(loading);
+
+    Align::centered(layout)
 }
