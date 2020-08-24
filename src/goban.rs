0491916666271a7e914f901793f8529935ff1c4d
Arttaaz <arttaaz@gmail.com>
2020-08-24 17:20:56 +0200

    Can now place stones on the board

--- src/goban.rs
+++ src/goban.rs
@@ -1,14 +1,97 @@
 use druid::widget::prelude::*;
-use druid::Color;
+use druid::{Color, MouseButton, Point};
 use druid::kurbo::{Line, Circle, Rect};
+use crate::Player;
+
+#[derive(Debug, Clone)]
+pub struct Stone {
+    pub coord: Point,
+    pub color: Color,
+}
+
+impl PartialEq for Stone {
+    fn eq(&self, oth: &Stone) -> bool {
+        if self.coord == oth.coord {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl Stone {
+    pub fn black(coord: Point) -> Self {
+        Self {
+            coord,
+            color: Color::BLACK,
+        }
+    }
+
+    pub fn white(coord: Point) -> Self {
+        Self {
+            coord,
+            color: Color::WHITE,
+        }
+    }
+
+    fn draw(&self, ctx: &mut PaintCtx, rect: &Rect, size: f64) {
+        ctx.fill(Circle::new((rect.x0 + self.coord.x * size/20.0, rect.y0 + self.coord.y * size/20.0), size/41.8), &self.color);
+        ctx.stroke(Circle::new((rect.x0 + self.coord.x * size/20.0, rect.y0 + self.coord.y * size/20.0), size/41.8), &Color::BLACK, 1.0);
+    }
+
+    fn hover(&self, ctx: &mut PaintCtx, rect: &Rect, size: f64) {
+        if self.color.as_rgba_u32() == Color::BLACK.as_rgba_u32() {
+            ctx.fill(Circle::new((rect.x0 + self.coord.x * size/20.0, rect.y0 + self.coord.y * size/20.0), size/41.8), &Color::BLACK.with_alpha(0.7));
+        } else if self.color.as_rgba_u32() == Color::WHITE.as_rgba_u32() {
+            ctx.fill(Circle::new((rect.x0 + self.coord.x * size/20.0, rect.y0 + self.coord.y * size/20.0), size/41.8), &Color::WHITE.with_alpha(0.7));
+        }
+        ctx.stroke(Circle::new((rect.x0 + self.coord.x * size/20.0, rect.y0 + self.coord.y * size/20.0), size/41.8), &Color::BLACK, 1.0);
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct Goban {
+    pub stones: Vec<Stone>, //The points coord are used as the goban coords for the stones to be placed
+    pub hover: Option<Stone>,
 }
 
 
 impl Widget<crate::RootState> for Goban {
-    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut crate::RootState, _env: &Env) {}
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut crate::RootState, _env: &Env) {
+        match event {
+            Event::MouseMove(mouse_event) => {
+                if ctx.is_hot() {
+                    let size = ctx.size();
+                    let rect = Rect::from_origin_size((0.0,0.0), size).contained_rect_with_aspect_ratio(1.0);
+                    let size = rect.height();
+                    let mut pos = mouse_event.pos;
+                    pos.x = ((pos.x - rect.x0) / (size/20.0)).round();
+                    pos.y = ((pos.y - rect.y0) / (size/20.0)).round();
+                    if pos.x > 0.0 && pos.y > 0.0 &&
+                       pos.x < 20.0 && pos.y < 20.0 {
+                       self.hover = match data.turn {
+                            Player::Black => Some(Stone::black(pos)),
+                            Player::White => Some(Stone::white(pos)),
+                        };
+                    } else {
+                        self.hover = None;
+                    }
+                } else {
+                    self.hover = None;
+                }
+                ctx.request_paint();
+            },
+            Event::MouseUp(mouse_event) => {
+                if mouse_event.button == MouseButton::Left && self.hover.is_some() && !self.stones.contains(self.hover.as_ref().unwrap()) {
+                    self.stones.push(self.hover.as_ref().unwrap().clone());
+                    self.hover = None;
+                    data.turn.next();
+                    ctx.request_paint();
+                }
+            }
+            _ => (),
+        }
+    }
 
     fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &crate::RootState, _env: &Env) {}
 
@@ -20,13 +103,14 @@ impl Widget<crate::RootState> for Goban {
 
     fn paint(&mut self, ctx: &mut PaintCtx, _data: &crate::RootState, _env: &Env) {
         let rect = Rect::from(ctx.region().to_rect().contained_rect_with_aspect_ratio(1.0));
+
         let size = rect.height();
         let fill_color = Color::rgb8(219, 185, 52);
 
         ctx.fill(rect, &fill_color);
 
-        let horizontal_lines: Vec<Line> = (1..20).map(|x| 
-            Line::new((rect.x0 + size/20.0, rect.y0 + (x as f64 * (size/20.0))), 
+        let horizontal_lines: Vec<Line> = (1..20).map(|x|
+            Line::new((rect.x0 + size/20.0, rect.y0 + (x as f64 * (size/20.0))),
                 (rect.x1- size/20.0, rect.y0 + (x as f64 * (size/20.0))))).collect();
         for line in horizontal_lines {
             ctx.stroke(line, &Color::BLACK, 1.5);
@@ -49,5 +133,13 @@ impl Widget<crate::RootState> for Goban {
         ctx.stroke(Circle::new((rect.x1 - 4.0 * size/20.0, rect.y0 + 10.0 * size/20.0), size/400.0), &Color::BLACK, size/400.0);
         ctx.stroke(Circle::new((rect.x0 + 10.0 * size/20.0, rect.y1 - 4.0 * size/20.0), size/400.0), &Color::BLACK, size/400.0);
         ctx.stroke(Circle::new((rect.x0 + 10.0 * size/20.0, rect.y0 + 10.0 * size/20.0), size/400.0), &Color::BLACK, size/400.0);
+
+        if self.hover.is_some() {
+            self.hover.as_ref().unwrap().hover(ctx, &rect, size);
+        }
+
+        self.stones.iter().for_each(|p| {
+            p.draw(ctx, &rect, size);
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
-extern crate pretty_env_logger;
-#[macro_use] 
-extern crate log;
-
-use druid::widget::{Align, Flex, Label};
+use log::info;
+use druid::widget::{Flex, Label};
 use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WindowDesc};
+use druid::Point;
 
 mod goban;
 use goban::Goban;
-
-const HORIZONTAL_WIDGET_SPACING: f64 = 0.1;
+use goban::Stone;
+const HORIZONTAL_WIDGET_SPACING: f64 = 0.1; // Flex factor
 const VERTICAL_WIDGET_SPACING: f64 = 20.0;
 const WINDOW_TITLE: LocalizedString<RootState> = LocalizedString::new("AInalyzer!");
+
+#[derive(Debug, Clone, Data, PartialEq)]
+enum Player {
+    Black,
+    White,
+}
+
+impl Player {
+    fn next(&mut self) {
+        match self {
+            Self::Black => *self = Self::White,
+            Self::White => *self = Self::Black,
+        }
+    }
+}
 
 #[derive(Debug, Clone, Data, Lens)]
 struct RootState {
     text: String,
+    pub turn: Player,
 }
 
 fn main() {
-    pretty_env_logger::init();
+    scrub_log::init().unwrap();
     info!("Starting the app");
     let main_window = WindowDesc::new(build_root_widget)
         .title(WINDOW_TITLE)
@@ -28,10 +42,9 @@ fn main() {
     AppLauncher::with_window(main_window)
         .launch(RootState{
             text: "AInalyzer".to_string(),
+            turn: Player::Black,
         })
         .expect("failed to launch app");
-
-    
 }
 
 fn build_root_widget() -> impl Widget<RootState> {
@@ -40,14 +53,17 @@ fn build_root_widget() -> impl Widget<RootState> {
         .with_spacer(VERTICAL_WIDGET_SPACING)
         .with_child(label)
         .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_flex_child(Goban {}, 1.0)
+        .with_flex_child(Goban {
+                            stones: vec![Stone::black(Point::new(10.0, 10.0)), Stone::black(Point::new(9.0, 9.0)),
+                                         Stone::white(Point::new(10.0, 9.0)), Stone::white(Point::new(9.0, 10.0))],
+                            hover: None,
+                        }, 1.0)
         .with_spacer(VERTICAL_WIDGET_SPACING);
 
-    let layout_centered = Flex::row()
+    Flex::row()
         .with_flex_spacer(HORIZONTAL_WIDGET_SPACING)
         .with_flex_child(layout, 1.0)
-        .with_flex_spacer(HORIZONTAL_WIDGET_SPACING);
+        .with_flex_spacer(HORIZONTAL_WIDGET_SPACING)
 
-    Align::centered(layout_centered)
 }
 
